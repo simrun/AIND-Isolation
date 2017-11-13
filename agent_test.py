0d33b8bb8cc07c92ea33cb2802e3a7a27d2cc96e
Simrun Basuita <hello@sim.run>
2017-11-13 21:07:13 +0000

    Add test for minimax with improved scoring

--- agent_test.py
+++ agent_test.py
@@ -10,7 +10,7 @@ import game_agent
 
 from importlib import reload
 
-from sample_players import center_score
+from sample_players import center_score, improved_score
 
 
 class IsolationTest(unittest.TestCase):
@@ -22,7 +22,7 @@ class IsolationTest(unittest.TestCase):
         self.player2 = "Player2"
         self.game = isolation.Board(self.player1, self.player2)
 
-    def testMiniMax(self):
+    def testMiniMaxCenter(self):
         time_left = lambda: 1000
 
         player1 = game_agent.MinimaxPlayer(1, center_score)
@@ -36,6 +36,21 @@ class IsolationTest(unittest.TestCase):
         assert(player1 == game.active_player)
 
         self.assertFalse(player1.get_move(game, time_left) == (2,2))
+
+    def testMiniMaxImproved(self):
+        time_left = lambda: 1000
+
+        player1 = game_agent.MinimaxPlayer(1, improved_score)
+        player2 = game_agent.MinimaxPlayer(1, improved_score)
+
+        game = isolation.Board(player1, player2, 9, 9)
+        game._board_state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+                             1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0,
+                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30, 24]
+
+        assert(player1 == game.active_player)
+
+        self.assertFalse(player1.get_move(game, time_left) == (7,0))
 
 if __name__ == '__main__':
     unittest.main()
